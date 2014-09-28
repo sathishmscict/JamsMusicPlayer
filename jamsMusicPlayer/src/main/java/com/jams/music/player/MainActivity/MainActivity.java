e903373f7788b5e53cde656bd01448d86d262f63
Salama A.B <devaksal@gmail.com>
2014-09-28 12:56:02 +0300

    Reset action bar; close left drawer on back press

--- jamsMusicPlayer/src/main/java/com/jams/music/player/MainActivity/MainActivity.java
+++ jamsMusicPlayer/src/main/java/com/jams/music/player/MainActivity/MainActivity.java
@@ -299,6 +299,11 @@ public class MainActivity extends FragmentActivity {
 	 * Loads the specified fragment into the target layout.
 	 */
 	public void switchContent(Fragment fragment) {
+        // Reset action bar
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setDisplayShowHomeEnabled(true);
+        getActionBar().setDisplayShowCustomEnabled(false);
+
 		getSupportFragmentManager().beginTransaction()
 								   .replace(R.id.mainActivityContainer, fragment)
 								   .commit();
@@ -510,13 +515,19 @@ public class MainActivity extends FragmentActivity {
 
     @Override
     public void onBackPressed() {
-        if (getCurrentFragmentId()==Common.FOLDERS_FRAGMENT)
+
+        if(mDrawerLayout.isDrawerOpen(Gravity.START)) { // Close left drawer if opened
+            mDrawerLayout.closeDrawer(Gravity.START);
+
+        } else if (getCurrentFragmentId()==Common.FOLDERS_FRAGMENT) {
             if (((FilesFoldersFragment) mCurrentFragment).getCurrentDir().equals("/"))
                 super.onBackPressed();
             else
                 ((FilesFoldersFragment) mCurrentFragment).getParentDir();
-        else
+
+        } else {
             super.onBackPressed();
+        }
 
     }
 
